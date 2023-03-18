80aaa5c54f0cf895e393ff59aa0c6eaee4afbba6
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-03-18 14:18:14 -0300

    criação dos links de nav e correções

--- src/Components/Banner/index.js
+++ src/Components/Banner/index.js
@@ -4,9 +4,18 @@ export default function Banner() {
     return (
         <section>
             <div className="d-flex flex-column align-items-center justify-content-center">
+                <div className='d-flex align-items-center justify-content-center navegacao'>
+                    <a className="nav-link" href="#">Sobre</a>
+                    <a className="nav-link" href="#">Links</a>
+                    <a className="nav-link" href="#">Contato</a>
+                </div>
+
                 <img className="img-fluid" src="./assets/img/priscila-1.svg" alt="Priscila" />
                 <h3>@priscilacruz_</h3>
+
+
             </div>
+
         </section>
     )
 }
