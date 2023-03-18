92b8762482c1a17857fe4cd2728d770cf15df18d
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-03-18 09:54:19 -0300

    criado o banner

---
+++ src/Components/Banner/index.js
@@ -0,0 +1,12 @@
+import styles from './Banner.css'
+
+export default function Banner() {
+    return (
+        <section>
+            <div className="d-flex flex-column align-items-center justify-content-center">
+                <img className="img-fluid" src="./assets/img/priscila-1.svg" className="img-fluid" alt="Priscila" />
+                <h3>@PriscilaCruz</h3>
+            </div>
+        </section>
+    )
+}
