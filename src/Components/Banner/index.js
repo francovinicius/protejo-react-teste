7879db6913dbd0bca78d11e17cce831def8c928c
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-03-18 10:05:08 -0300

    alterações

--- src/Components/Banner/index.js
+++ src/Components/Banner/index.js
@@ -4,7 +4,7 @@ export default function Banner() {
     return (
         <section>
             <div className="d-flex flex-column align-items-center justify-content-center">
-                <img className="img-fluid" src="./assets/img/priscila-1.svg" className="img-fluid" alt="Priscila" />
+                <img className="img-fluid" src="./assets/img/priscila-1.svg" alt="Priscila" />
                 <h3>@PriscilaCruz</h3>
             </div>
         </section>
